7a27c2175a50acca23e271c227ef33d6797d0bb2
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2024-04-13 14:05:00 -0700

    Update Projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,20 +5,27 @@ import {useRef} from "react";
 const items = [
     {
         id:1,
+        title: 'Image Classification Using CNN',
+        img:'https://images.pexels.com/photos/17483874/pexels-photo-17483874/free-photo-of-an-artist-s-illustration-of-artificial-intelligence-ai-this-image-was-inspired-by-neural-networks-used-in-deep-learning-it-was-created-by-novoto-studio-as-part-of-the-visualising-ai-pr.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        desc:'Developed and trained a CNN-based multi-class image classification Deep Learning model using TensorFlow and OpenCV to categorize images into distinct classes.',
+        link:'https://github.com/nehasawant4/Image-Classification-Using-CNN',
+    },
+    {
+        id:2,
         title: 'React: Blogging App',
         img:'https://images.pexels.com/photos/1092671/pexels-photo-1092671.jpeg?auto=compress&cs=tinysrgb&w=800',
         desc:'A full-stack blog app with responsive UI and interactive UX developed with React, Next.js, CSS, MongoDB, etc. The app supports CRUD operations for blogs and comments, filtering of blogs based on categories and users, pagination, Google OAuth for Google Sign-In, etc.',
         link:'https://github.com/nehasawant4/Blogging-App-React',
     },
     {
-        id:2,
+        id:3,
         title: 'Flutter: Bilingual Math App',
         img:'https://images.pexels.com/photos/167682/pexels-photo-167682.jpeg?auto=compress&cs=tinysrgb&w=800',
         desc:'A mobile app for iOS/Android designed meticulously in Figma and developed using Flutter and Firebase, intended to interactively teach mathematics, assisting Hispanic children to overcome language barriers. Features include quizzes, games, flashcards, text-to-speech, streak calendar, etc.',
         link:'https://github.com/nehasawant4/Bilingual-Maths-Flutter-App',
     },
     {
-        id:3,
+        id:4,
         title: 'AWS: PPE Detection System',
         img:'https://images.pexels.com/photos/1108101/pexels-photo-1108101.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         desc:'Developed a Java web application integrated with AWS for detection of Personal Protective Equipment and monitoring its compliance on custom building premises. Used Amazon Rekognition for detection of PPE in images. Additionally used services like Amazon S3, DynamoDB, etc.',
