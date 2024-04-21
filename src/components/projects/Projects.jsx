4a6d1a44315e95a60771df2c6fa7a6b42b98f0cc
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2024-04-21 10:44:57 -0700

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -12,9 +12,9 @@ const items = [
     },
     {
         id:2,
-        title: 'Applicant Tracking System Using Google Gemini Pro Vision',
+        title: 'Applicant Tracking System Using Google Gemini Pro',
         img:'https://blogimages.softwaresuggest.com/blog/wp-content/uploads/2024/02/09114343/Features-of-Applicant-Tracking-System.jpg',
-        desc:'The system is designed to assist the recruitment process providing an efficient solution for employers to screen resumes while empowering job seekers to optimize their resumes boosting their chances of securing a job. It evaluates resumes in comparison to job descriptions, identifies candidates\' strengths and weaknesses, and provides tailored suggestions for resume enhancement. Additionally, the system calculates a match percentage and pinpoints missing keywords, aiding candidates in refining their applications to better align with job requirements.',
+        desc:'The system is designed to assist the recruitment process providing an efficient solution for employers to screen resumes while empowering job seekers to optimize their resumes boosting their chances of securing a job.',
         link:'https://github.com/nehasawant4/ATS-GenAI-Google-Gemini',
     },
     {
