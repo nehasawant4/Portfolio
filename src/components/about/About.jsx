ccac791186eb7219c9bc265e89ca54a26002e9a7
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2025-05-21 19:20:59 -0700

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,26 +1,9 @@
 import "./about.scss"
-import {motion} from "framer-motion";
 
-const sliderVariants = {
-    initial:{
-        x:0,
-    },
-    animate:{
-        x:"-600%",
-        transition:{
-            repeat: Infinity,
-            repeatType: "mirror",
-            duration: 15,
-        },
-    },
-}
 const About = () => {
     return(
         <div className="about">
             <div className="wrapper">
-                {/* <motion.div className="slidingText" variants={sliderVariants} initial="initial" animate="animate">
-                    Machine Learning | Cloud Computing | Full-Stack Development | UI/UX Design
-                </motion.div> */}
                 <div className="imageContainer">
                     <img src="/neha.png" alt=""/>
                 </div>
